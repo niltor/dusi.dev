1b104886134d526728cd6a737f5a25c7bf7ff59d
niltor <zpty@outlook.com>
2022-09-14 16:53:16 +0800

    try to update to .net7.0

--- src/Application/Implement/CommandStoreBase.cs
+++ src/Application/Implement/CommandStoreBase.cs
@@ -1,4 +1,3 @@
-using EFCore.BulkExtensions;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 
 namespace Application.Implement;
@@ -136,7 +135,7 @@ public class CommandStoreBase<TContext, TEntity> : ICommandStore<TEntity>, IComm
     /// <returns></returns>
     public virtual async Task<int> UpdateRangeAsync<TUpdate>(Expression<Func<TEntity, bool>> whereExp, TUpdate dto)
     {
-        return await _db.Where(whereExp).BatchUpdateAsync(dto!);
+        //return await _db.Where(whereExp).BatchUpdateAsync(dto!);
     }
 
     /// <summary>
@@ -148,7 +147,7 @@ public class CommandStoreBase<TContext, TEntity> : ICommandStore<TEntity>, IComm
     /// <returns></returns>
     public virtual async Task<int> EditRangeAsync<TUpdate>(List<Guid> ids, TUpdate dto)
     {
-        return await _db.Where(d => ids.Contains(d.Id)).BatchUpdateAsync(dto!);
+        //return await _db.Where(d => ids.Contains(d.Id)).BatchUpdateAsync(dto!);
     }
 
     /// <summary>
@@ -158,7 +157,7 @@ public class CommandStoreBase<TContext, TEntity> : ICommandStore<TEntity>, IComm
     /// <returns></returns>
     public virtual async Task<int> DeleteRangeAsync(List<Guid> ids)
     {
-        return await _db.Where(d => ids.Contains(d.Id)).BatchDeleteAsync();
+        //return await _db.Where(d => ids.Contains(d.Id)).BatchDeleteAsync();
     }
 
     /// <summary>
@@ -168,7 +167,7 @@ public class CommandStoreBase<TContext, TEntity> : ICommandStore<TEntity>, IComm
     /// <returns></returns>
     public virtual async Task<int> DeleteRangeAsync(Expression<Func<TEntity, bool>> whereExp)
     {
-        return await _db.Where(whereExp).BatchDeleteAsync();
+        //return await _db.Where(whereExp).BatchDeleteAsync();
     }
 }
 public class CommandSet<TEntity> : CommandStoreBase<CommandDbContext, TEntity>
---
+++ src/Core/Entities/EntityDesign/EntityLibrary.cs
@@ -0,0 +1,8 @@
+﻿namespace Core.Entities.EntityDesign;
+/// <summary>
+/// 实体库
+/// </summary>
+public class EntityLibrary
+{
+    public User User { get; set; }
+}
