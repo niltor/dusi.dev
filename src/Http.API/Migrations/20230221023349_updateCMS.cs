5a464f80c0e0c27cd0799e711b952019143a0c48
niltor <zpty@outlook.com>
2023-02-21 10:39:34 +0800

    更新迁移文件

---
+++ src/Http.API/Migrations/20230221023349_updateCMS.cs
@@ -0,0 +1,216 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Http.API.Migrations
+{
+    /// <inheritdoc />
+    public partial class updateCMS : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<Guid>(
+                name: "CatalogId",
+                table: "Blogs",
+                type: "uuid",
+                nullable: false,
+                defaultValue: new Guid("00000000-0000-0000-0000-000000000000"));
+
+            migrationBuilder.AddColumn<bool>(
+                name: "IsOriginal",
+                table: "Blogs",
+                type: "boolean",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "IsPublic",
+                table: "Blogs",
+                type: "boolean",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.CreateTable(
+                name: "Catalogs",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    CreatedTime = table.Column<DateTimeOffset>(type: "timestamp with time zone", nullable: false),
+                    UpdatedTime = table.Column<DateTimeOffset>(type: "timestamp with time zone", nullable: false),
+                    IsDeleted = table.Column<bool>(type: "boolean", nullable: false),
+                    Name = table.Column<string>(type: "character varying(50)", maxLength: 50, nullable: false),
+                    Level = table.Column<short>(type: "smallint", nullable: false),
+                    ParentId = table.Column<Guid>(type: "uuid", nullable: true),
+                    UserId = table.Column<Guid>(type: "uuid", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Catalogs", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Catalogs_Catalogs_ParentId",
+                        column: x => x.ParentId,
+                        principalTable: "Catalogs",
+                        principalColumn: "Id");
+                    table.ForeignKey(
+                        name: "FK_Catalogs_Users_UserId",
+                        column: x => x.UserId,
+                        principalTable: "Users",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Tags",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    CreatedTime = table.Column<DateTimeOffset>(type: "timestamp with time zone", nullable: false),
+                    UpdatedTime = table.Column<DateTimeOffset>(type: "timestamp with time zone", nullable: false),
+                    IsDeleted = table.Column<bool>(type: "boolean", nullable: false),
+                    Name = table.Column<string>(type: "character varying(50)", maxLength: 50, nullable: false),
+                    Color = table.Column<string>(type: "character varying(20)", maxLength: 20, nullable: true),
+                    UserId = table.Column<Guid>(type: "uuid", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Tags", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Tags_Users_UserId",
+                        column: x => x.UserId,
+                        principalTable: "Users",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "BlogTags",
+                columns: table => new
+                {
+                    BlogsId = table.Column<Guid>(type: "uuid", nullable: false),
+                    TagsId = table.Column<Guid>(type: "uuid", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_BlogTags", x => new { x.BlogsId, x.TagsId });
+                    table.ForeignKey(
+                        name: "FK_BlogTags_Blogs_BlogsId",
+                        column: x => x.BlogsId,
+                        principalTable: "Blogs",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_BlogTags_Tags_TagsId",
+                        column: x => x.TagsId,
+                        principalTable: "Tags",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Blogs_CatalogId",
+                table: "Blogs",
+                column: "CatalogId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Blogs_IsOriginal",
+                table: "Blogs",
+                column: "IsOriginal");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Blogs_IsPublic",
+                table: "Blogs",
+                column: "IsPublic");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_BlogTags_TagsId",
+                table: "BlogTags",
+                column: "TagsId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Catalogs_Level",
+                table: "Catalogs",
+                column: "Level");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Catalogs_Name",
+                table: "Catalogs",
+                column: "Name");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Catalogs_ParentId",
+                table: "Catalogs",
+                column: "ParentId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Catalogs_UserId",
+                table: "Catalogs",
+                column: "UserId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Tags_Color",
+                table: "Tags",
+                column: "Color");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Tags_Name",
+                table: "Tags",
+                column: "Name");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Tags_UserId",
+                table: "Tags",
+                column: "UserId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Blogs_Catalogs_CatalogId",
+                table: "Blogs",
+                column: "CatalogId",
+                principalTable: "Catalogs",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Blogs_Catalogs_CatalogId",
+                table: "Blogs");
+
+            migrationBuilder.DropTable(
+                name: "BlogTags");
+
+            migrationBuilder.DropTable(
+                name: "Catalogs");
+
+            migrationBuilder.DropTable(
+                name: "Tags");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Blogs_CatalogId",
+                table: "Blogs");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Blogs_IsOriginal",
+                table: "Blogs");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Blogs_IsPublic",
+                table: "Blogs");
+
+            migrationBuilder.DropColumn(
+                name: "CatalogId",
+                table: "Blogs");
+
+            migrationBuilder.DropColumn(
+                name: "IsOriginal",
+                table: "Blogs");
+
+            migrationBuilder.DropColumn(
+                name: "IsPublic",
+                table: "Blogs");
+        }
+    }
+}
