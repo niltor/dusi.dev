2fc754fdf532d4cbe7bb8ceaf2c1022ddf08406b
niltor <zpty@outlook.com>
2023-02-21 18:09:42 +0800

    update ng blog route

--- src/Http.API/ClientApp/src/app/pages/workspace/blog/blog-routing.module.ts
+++ src/Http.API/ClientApp/src/app/pages/workspace/blog/blog-routing.module.ts
@@ -6,12 +6,13 @@ import { AddComponent } from './add/add.component';
 import { DetailComponent } from './detail/detail.component';
 import { EditComponent } from './edit/edit.component';
 import { LayoutComponent } from '../layout/layout.component';
+import { SettingComponent } from './setting/setting.component';
 
 const routes: Routes = [
   {
     path: 'workspace',
     component: LayoutComponent,
-    data: {reuse: true},
+    data: { reuse: true },
     canActivate: [AuthGuard],
     children:
       [
@@ -20,6 +21,7 @@ const routes: Routes = [
           canActivateChild: [AuthGuard],
           children: [
             { path: '', pathMatch: 'full', redirectTo: 'index' },
+            { path: 'setting', component: SettingComponent },
             { path: 'index', component: IndexComponent },
             { path: 'add', component: AddComponent },
             { path: 'detail/:id', component: DetailComponent },
