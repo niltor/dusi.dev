bc0543507b650ea70a7f3fd7e35c2da540e13dd1
niltor <zpty@outlook.com>
2023-04-24 11:19:15 +0800

    add opensource entity

---
+++ src/Core/Entities/CMS/OpenSourceProduct.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Core.Entities.CMS;
+/// <summary>
+/// 开源作品
+/// </summary>
+[Index(nameof(Title))]
+public sealed class OpenSourceProduct : EntityBase
+{
+    /// <summary>
+    /// 标题
+    /// </summary>
+    [MaxLength(100)]
+    public required string Title { get; set; }
+    /// <summary>
+    /// project url address
+    /// </summary>
+    [MaxLength(200)]
+    public required string ProjectUrl { get; set; }
+    /// <summary>
+    /// 描述
+    /// </summary>
+    [MaxLength(500)]
+    public required string Description { get; set; }
+
+    /// <summary>
+    /// 缩略图
+    /// </summary>
+    [MaxLength(200)]
+    public string? Thumbnail { get; set; }
+    /// <summary>
+    /// 作者
+    /// </summary>
+    [MaxLength(200)]
+    public string? Authors { get; set; }
+    /// <summary>
+    /// 标签
+    /// </summary>
+    [MaxLength(300)]
+    public string? Tags { get; set; }
+}
+
+
