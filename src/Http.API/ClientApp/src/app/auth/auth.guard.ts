003d40bff8824412cb199f69a92a90237917726d
niltor <zpty@outlook.com>
2023-05-06 21:37:20 +0800

    调整dockerfile

--- src/Http.API/ClientApp/src/app/auth/auth.guard.ts
+++ src/Http.API/ClientApp/src/app/auth/auth.guard.ts
@@ -1,12 +1,12 @@
 import { Injectable } from '@angular/core';
-import { ActivatedRouteSnapshot, CanActivate, Router, RouterStateSnapshot, UrlTree } from '@angular/router';
+import { ActivatedRouteSnapshot, Router, RouterStateSnapshot, UrlTree } from '@angular/router';
 import { map, Observable } from 'rxjs';
 import { LoginService } from './login.service';
 
 @Injectable({
   providedIn: 'root'
 })
-export class AuthGuard implements CanActivate {
+export class AuthGuard  {
   constructor(
     private router: Router,
     private auth: LoginService,
