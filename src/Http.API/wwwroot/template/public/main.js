393cdf6e4de778c41ee72a14ecba612b364e07fb
niltor <zpty@outlook.com>
2023-05-08 22:24:28 +0800

    调整main.js

--- src/Http.API/wwwroot/template/public/main.js
+++ src/Http.API/wwwroot/template/public/main.js
@@ -1,8 +1,9 @@
+export default {
+  // after dom render ready
+  window.onload = function () {
+    const tagA = document.getElementsByClassName("navbar-brand")[0];
+    tagA.setAttribute("href", "/");
+    document.getElementsByClassName("actionbar")[0].style.display = "none";
+  }
 
-// after dom render ready
-window.onload = function () {
-  const tagA = document.getElementsByClassName("navbar-brand")[0];
-  tagA.setAttribute("href", "/");
-  document.getElementsByClassName("actionbar")[0].style.display = "none";
 }
-
